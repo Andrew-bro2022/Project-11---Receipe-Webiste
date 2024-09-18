06630ba24399c9ac49b5d505fd25901b03d3c54e
Andy Zheng <andyzheng.ca@gmail.com>
2024-09-17 22:16:11 -0400

    update new tab for clicking View Details

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,25 +1,49 @@
-
-
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 const RecipeCard = ({ recipe }) => {
+  const handleViewDetails = () => {
+    const url = `/recipe/${recipe.id}`;
+    window.open(url, '_blank'); // Opens the details page in a new tab
+  };
+
   return (
     <Card className="h-100">
       <Card.Img variant="top" src={recipe.image} alt={recipe.title} />
       <Card.Body>
         <Card.Title>{recipe.title}</Card.Title>
-        <Link to={`/recipe/${recipe.id}`}>
-          <Button variant="primary" className="mt-2">View Details</Button>
-        </Link>
+        <Button variant="primary" className="mt-2" onClick={handleViewDetails}>
+          View Details
+        </Button>
       </Card.Body>
     </Card>
   );
 };
 
 export default RecipeCard;
+
+
+
+// import React from 'react';
+// import { Card, Button } from 'react-bootstrap';
+// import { Link } from 'react-router-dom';
+// // import 'bootstrap/dist/css/bootstrap.min.css'; 
+
+// const RecipeCard = ({ recipe }) => {
+//   return (
+//     <Card className="h-100">
+//       <Card.Img variant="top" src={recipe.image} alt={recipe.title} />
+//       <Card.Body>
+//         <Card.Title>{recipe.title}</Card.Title>
+//         <Link to={`/recipe/${recipe.id}`}>
+//           <Button variant="primary" className="mt-2">View Details</Button>
+//         </Link>
+//       </Card.Body>
+//     </Card>
+//   );
+// };
+
+// export default RecipeCard;
 
 
 
